2d6138c38d140b30fdeeec2bea2018c52d6e9af1
imdavyking <yungdynamic53@gmail.com>
2026-05-11 11:50:58 +0100

    chore: updated

---
+++ backend/prova/solana_gen_vk/src/main.rs
@@ -0,0 +1,25 @@
+use dotenv::dotenv;
+use gnark_verifier_solana::vk::generate_key_file;
+use std::env;
+use std::path::Path;
+pub mod poseidon2;
+pub mod vk;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-env-changed=VK_PATH");
+    dotenv().ok();
+
+    // Default VK path relative to crate root
+    let default = "default.vk";
+    let vk_path = env::var("VK_PATH").unwrap_or_else(|_| default.to_string());
+
+    if Path::new(&vk_path).exists() {
+        println!("cargo:rerun-if-changed={}", vk_path);
+    }
+
+    if let Err(e) = generate_key_file(&vk_path, "src/vk.rs") {
+        panic!("Failed to generate key file '{}': {e}", vk_path);
+    }
+
+    Ok(())
+}
